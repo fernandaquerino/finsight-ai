e7d1700913d7aebe47841605935b9abc15c51fa0
fernandaquerino <fequerinodelima@gmail.com>
2026-06-01 06:28:52 -0300

    feat: add notifications item component to notifications panel

--- src/components/app/Topbar/Topbar.test.tsx
+++ src/components/app/Topbar/Topbar.test.tsx
@@ -28,7 +28,7 @@ describe("Topbar", () => {
       screen.getByRole("button", { name: "Alternar tema" }),
     ).toBeInTheDocument();
     expect(
-      screen.getByRole("button", { name: "Notificações" }),
+      screen.getByRole("button", { name: /Notificações/ }),
     ).toBeInTheDocument();
     expect(screen.getByRole("img", { name: "Marina Rocha" })).toBeVisible();
   });
